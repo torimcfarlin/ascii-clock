c478c34c0a5ce44d1cfa7fecccb6b4444215a0c3
Tori McFarlin <tmcfarlin@Toris-MBP.gateway>
2016-05-29 22:11:42 +1000

    Update go script and README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ascii-clock
 A program which displays the time as an ascii clock
+
+
+# Run it
+First, set up your virtual environment
+
+$ pip install virtualenv
+
+$ virtualenv ascii-clock
+
+$ python setup.py develop
+
+Then run the go script:
+
+$ python go.py
+
+'21:35'
+
+     o
+  o     o
+ o       o
+h         o
+ o       o
+  m     o
+     o
--- go.py
+++ go.py
@@ -1,10 +1,26 @@
 import click
 
+from ascii_clock.clock import time_to_clock, validate_time, TimeValidationError
 
-@click.group
-def go():
-    pass
+
+def get_time():
+    time = None
+    while time is None:
+        time = str(input(''))
+        try:
+            validate_time(time)
+        except TimeValidationError as e:
+            print e.message
+            time = None
+    return time
+
+
+@click.command()
+def clock():
+    time = get_time()
+    clock = time_to_clock(time)
+    print clock
 
 
 if __name__ == '__main__':
-    go()
+    clock()
